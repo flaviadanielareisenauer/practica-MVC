e1beba23e74b00a52ed72be4cc78898d131e4167
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2021-12-14 23:08:06 -0300

    agregando archivos base

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const mainRouter = require("./router/main");
+
+
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Servidor funcionando");
